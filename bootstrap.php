1ca3b1246655fd7cd29fe747be04aafdea30d614
Alex Groleau <alex@teampalmtree.com>
2013-10-19 23:20:02 -0400

    bug fixes

---
+++ bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+// add views folder
+Finder::instance()->add_path(__DIR__);
--- classes/controller/promoter.php
+++ classes/controller/promoter.php
@@ -5,7 +5,7 @@ class Controller_Promoter extends Controller_Template
 
     public function action_login($user_id)
     {
-
+        Auth::force_login($user_id);
     }
 
 }
