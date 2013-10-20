8ab0de263994b8a4efce76b315be4604a5b773e5
Alex Groleau <alex@teampalmtree.com>
2013-10-20 16:42:29 -0400

    bug fixes

--- bootstrap.php
+++ bootstrap.php
@@ -2,3 +2,7 @@
 
 // add views folder
 Finder::instance()->add_path(__DIR__);
+
+Autoloader::add_classes(array(
+    'Controller_Promoter'   => __DIR__.'/classes/controller/promoter.php',
+));
--- classes/controller/promoter.php
+++ classes/controller/promoter.php
@@ -1,11 +1,16 @@
 <?php
 
-class Controller_Promoter extends Controller_Template
+class Controller_Promoter extends \Fuel\Core\Controller_Rest
 {
 
     public function action_login($user_id)
     {
+        // force login user
         Auth::force_login($user_id);
+        // get redirect url
+        $redirect_url = urldecode($_GET['redirect_url']);
+        // redirect :)
+        return Response::redirect($redirect_url, 'refresh');
     }
 
 }
