1de11d465ff63c7f4c90329930cf9e20d4f7d843
Alex Groleau <alex@teampalmtree.com>
2014-01-02 15:28:54 -0500

    shared login UI created shared auth controller created

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,6 @@
 Finder::instance()->add_path(__DIR__);
 
 Autoloader::add_classes(array(
+    'Controller_Auth'   => __DIR__.'/classes/controller/auth.php',
     'Controller_Promoter'   => __DIR__.'/classes/controller/promoter.php',
 ));
--- classes/controller/promoter.php
+++ classes/controller/promoter.php
@@ -1,9 +1,32 @@
 <?php
 
-class Controller_Promoter extends \Fuel\Core\Controller_Rest
+class Controller_Promoter extends \Fuel\Core\Controller_Hybrid
 {
 
-    public function action_login($user_id)
+    public function action_index()
+    {
+        return Response::forge(View::forge('promoter/index'));
+    }
+
+    public function action_login()
+    {
+        // if we got login data
+        if (Input::post())
+        {
+            // check credentials
+            if (Auth::login())
+            {
+                // Credentials ok, go right in.
+                Response::redirect('manager');
+                return;
+            }
+            else
+            {
+            }
+        }
+    }
+
+    public function action_callback($user_id)
     {
         // force login user
         Auth::force_login($user_id);
