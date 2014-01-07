69c5b652825a87be822c8a27314117b0d2ede898
Alex Groleau <alex@teampalmtree.com>
2014-01-06 19:52:09 -0500

    extended ormauth to allow forced logins without logouts of existing sessions

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,5 @@ Finder::instance()->add_path(__DIR__);
 Autoloader::add_classes(array(
     'Controller_Auth'   => __DIR__.'/classes/controller/auth.php',
     'Controller_Promoter'   => __DIR__.'/classes/controller/promoter.php',
+    'Auth_Login_Promoterauth' => __DIR__.'/classes/auth/login/promoterauth.php',
 ));
