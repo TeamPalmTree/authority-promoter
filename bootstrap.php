318dc621e7798942f1093a885da9ad3bbb2eed0a
Alex Groleau <alex@teampalmtree.com>
2014-01-18 15:24:01 -0500

    Model_User now Promoter_User Added Promoter_Metadata

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,8 @@ Finder::instance()->add_path(__DIR__);
 Autoloader::add_classes(array(
     'Controller_Auth'   => __DIR__.'/classes/controller/auth.php',
     'Controller_Promoter'   => __DIR__.'/classes/controller/promoter.php',
-    'Promoter\\Model\\Model_User' => __DIR__.'/classes/model/user.php',
     'Promoter' => __DIR__.'/classes/promoter.php',
     'HttpAccessDeniedException'   => __DIR__.'/classes/exceptions.php',
+    'Promoter\\Model\\Promoter_User' => __DIR__.'/classes/model/promoter/user.php',
+    'Promoter\\Model\\Promoter_Metadata' => __DIR__.'/classes/model/promoter/metadata.php',
 ));
